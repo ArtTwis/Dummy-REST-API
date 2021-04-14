63a5f8c71dd6abb4f710360ababf225d87028161
Twinkle Goyal <art.twis@gmail.com>
2021-04-14 18:13:13 +0530

    modify index page

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,23 @@
       if(__response){
         for(let resource of __response.data){
           resourceContainer.innerHTML += `
-          <div class="resourcesContentDv">
-            <div class="resourceMethodDv flex justify-center align-center">
-              <label class="resourceMethodLabel">${resource.method}</label>
-            </div>
-            <label class="resourceLabel">${resource.resourceName}</label>
+          <div data-methodName="${resource.method}" class="resourcesMethodDv resourceMethod">
+          <label data-methodName="${resource.method}" class="resourceMethod flex justify-center align-center">${resource.method}</label>
           </div>
           `;
+
+          (resource.list).forEach((__resource) => {
+            resourceContainer.innerHTML += `
+            <div data-resourceContent-method="${__resource.method}" class="hide resourcesContentParentDv flex justify-center align-center">
+            <div class="resourcesContentDv">
+              <div class="resourceMethodDv flex justify-center align-center">
+                <label class="resourceMethodLabel">${__resource.method}</label>
+              </div>
+              <label class="resourceLabel">${__resource.resourceName}</label>
+            </div>
+            </div>
+            `;
+          });
         }
       }
     }
@@ -23,6 +33,16 @@
   xhr.open('GET', 'http://localhost:1818/resources', true);
   xhr.send();
 })();
+
+document.addEventListener('click', (event) => {
+  if(event.target.classList.contains('resourceMethod')){
+    let __method = event.target.getAttribute('data-methodName');
+    let resourceElements = document.querySelectorAll(`div[data-resourceContent-method="${__method}"]`);
+    resourceElements.forEach((element) => {
+      element.classList.toggle('hide');
+    });
+  }
+});
 
 function setContainerSize(element){
   let height = element.offsetHeight;
