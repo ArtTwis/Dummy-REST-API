6000edb462407576ccee0e12fc3418546557e6bd
Twinkle Goyal <art.twis@gmail.com>
2021-04-14 16:04:15 +0530

    added assets.js file

---
+++ web/assets.js
@@ -0,0 +1,160 @@
+module.exports = {
+  "resources": [
+    {
+      "method": "GET",
+      "resourceName": "List Users" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "List Products" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "List Todos" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "List Posts" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "Single User" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "Single Product" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "Single Comment" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "Single Post" 
+    },
+    {
+      "method": "GET",
+      "resourceName": "Single Todo" 
+    },
+  ],
+  "users": [
+    {
+      "name": "Twinkle Goyal",
+      "sex": "Male"
+    },
+    {
+      "name": "Shivam Singla",
+      "sex": "Male"
+    },
+    {
+      "name": "Chanchal Goyal",
+      "sex": "Female"
+    },
+    {
+      "name": "Karan Aggarwal",
+      "sex": "Male"
+    },
+    {
+      "name": "Gagan Bansal",
+      "sex": "Male"
+    },
+    {
+      "name": "Nimrat Kaur",
+      "sex": "Female"
+    },
+    {
+      "name": "Sandeep Bansal",
+      "sex": "Male"
+    },
+    {
+      "name": "Sandeep Goyal",
+      "sex": "Male"
+    },
+    {
+      "name": "Shiwani Arora",
+      "sex": "Female"
+    },
+    {
+      "name": "Prince Bansal",
+      "sex": "Male"
+    },
+    {
+      "name": "Nitin Goyal",
+      "sex": "Male"
+    },
+    {
+      "name": "Shruti Jain",
+      "sex": "Female"
+    },
+    {
+      "name": "Vasu Singla",
+      "sex": "Male"
+    },
+    {
+      "name": "Nancy Gupta",
+      "sex": "Female"
+    },
+    {
+      "name": "Yatin Singla",
+      "sex": "Male"
+    },
+  ],
+  "todos": [
+    {
+      "userid": "11141XX14",
+      "title": "Create homepage"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Purchase stationary from market"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Bring dog food from supermarket"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Compete today's assignment"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Replace toothbrush"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Wash car"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Recharge dad's mobile"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Organise party for karan birthday"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Pack luggage for thailand trip"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Purchase wine bottle for gagan"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Buy airpods for indian friends"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Repair mini carvaan speaker"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Help pulkit in completing his task"
+    },
+    {
+      "userid": "11141XX14",
+      "title": "Create assignment for students"
+    },
+  ]
+}
