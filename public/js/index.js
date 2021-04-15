da5dcf0393b662ad06232161b04974213a0e5cf7
Twinkle Goyal <art.twis@gmail.com>
2021-04-15 08:06:48 +0530

    modify index files

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,9 @@
-const apiendpoint = 'http://localhost:1818/api';
+const apiendpoint = 'http://localhost:1818';
+
+(function(){
+  let responseBody_Tag = document.querySelector('#responseBody_tag');
+  setContainerSize(responseBody_Tag);
+})();
 
 (function(){
   let xhr = new XMLHttpRequest();
@@ -11,19 +16,19 @@ const apiendpoint = 'http://localhost:1818/api';
       if(__response){
         for(let resource of __response.data){
           resourceContainer.innerHTML += `
-          <div data-methodName="${resource.method}" class="resourcesMethodDv resourceMethod">
-            <label data-methodName="${resource.method}" class="resourceMethod flex justify-center align-center">${resource.method}</label>
+          <div data-method="${resource.method}" class="resourceMethod resourcesMethodDv">
+            <label data-method="${resource.method}" class="resourceMethod flex justify-center align-center">${resource.method}</label>
           </div>
           `;
 
           (resource.list).forEach((__resource) => {
             resourceContainer.innerHTML += `
-            <div data-resourceContent-method="${__resource.method}" data-resourceMethod="${__resource.method}" data-resourceContent-activity="${__resource.resourceName}}" class="hide resourcesContentParentDv flex justify-center align-center">
-              <div data-resourceMethod="${__resource.method}" data-resourceContent-activity="${__resource.resourceName}}" class="resourcesContentDv">
-                <div data-resourceMethod="${__resource.method}" data-resourceContent-activity="${__resource.resourceName}}" class="resourceMethodDv flex justify-center align-center">
-                  <label data-resourceMethod="${__resource.method}" data-resourceContent-activity="${__resource.resourceName}}" class="resourceMethodLabel">${__resource.method}</label>
+            <div data-method="${__resource.method}" data-endpoint="${__resource.endPoint}" data-resourcename="${__resource.resourceName}" class="hide actual_resource resourcesContentParentDv flex justify-center align-center">
+              <div data-method="${__resource.method}" data-endpoint="${__resource.endPoint}" data-resourcename="${__resource.resourceName}" class="actual_resource  resourcesContentDv">
+                <div data-method="${__resource.method}" data-endpoint="${__resource.endPoint}" data-resourcename="${__resource.resourceName}" class="actual_resource  resourceMethodDv flex justify-center align-center">
+                  <label data-method="${__resource.method}" data-endpoint="${__resource.endPoint}" data-resourcename="${__resource.resourceName}" class="actual_resource  resourceMethodLabel">${__resource.method}</label>
                 </div>
-                <label data-resourceMethod="${__resource.method}" data-resourceContent-activity="${__resource.resourceName}}" class="resourceLabel">${__resource.resourceName}</label>
+                <label data-method="${__resource.method}" data-endpoint="${__resource.endPoint}" data-resourcename="${__resource.resourceName}" class="actual_resource  resourceLabel">${__resource.resourceName}</label>
               </div>
             </div>
             `;
@@ -32,17 +37,51 @@ const apiendpoint = 'http://localhost:1818/api';
       }
     }
   }
-  xhr.open('GET', `${apiendpoint}/resources`, true);
+  xhr.open('GET', `${apiendpoint}/api/resources`, true);
   xhr.send();
 })();
 
 document.addEventListener('click', (event) => {
   if(event.target.classList.contains('resourceMethod')){
-    let __method = event.target.getAttribute('data-methodName');
-    let resourceElements = document.querySelectorAll(`div[data-resourceContent-method="${__method}"]`);
+    let __method = event.target.getAttribute('data-method');
+    let resourceElements = document.querySelectorAll(`.resourcesContentParentDv`);
     resourceElements.forEach((element) => {
-      element.classList.toggle('hide');
+      if(element.getAttribute('data-method') == __method) element.classList.toggle('hide');
+      else element.classList.add('hide');
     });
+  }
+
+  if(event.target.classList.contains('actual_resource')){
+    let method = event.target.getAttribute('data-method');
+    let endpoint = event.target.getAttribute('data-endpoint');
+    let resourceName = event.target.getAttribute('data-resourcename');
+    document.querySelector('#labelRequest_Value').textContent = endpoint;
+
+    // TODO: if request body/headers than show in request_contentDv work pending .....
+    document.querySelector('#request_contentDv').innerHTML = ``;
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        let __statusCode = this.status;
+        let statusCode_Element = document.querySelector('#labelResponse_Value');
+        
+        if(__statusCode >= 200 && __statusCode <= 300){
+          statusCode_Element.classList.add('darkGreen');
+        }else if(__statusCode >= 300 && __statusCode <= 400){
+          statusCode_Element.classList.add('darkOrange');
+        }else{
+          statusCode_Element.classList.add('darkRed');
+        }
+        statusCode_Element.textContent = __statusCode;
+        
+        let __resp = JSON.parse(this.response);
+        let responseBody_Element = document.querySelector('#responseBody_tag');
+        responseBody_Element.textContent = JSON.stringify(__resp, undefined, 2);
+      }
+    }
+    xhr.open(`${method}`, `${apiendpoint}${endpoint}`, true);
+    xhr.send();
   }
 });
 
