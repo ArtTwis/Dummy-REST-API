e99c26166f1e789ebeecbb69c63ce247af39bcfd
Twinkle Goyal <art.twis@gmail.com>
2021-04-19 06:08:46 +0530

    modify index fie

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,56 @@ const bodyObject = {};
 
   let responseBody_Tag = document.querySelector('#responseBody_tag');
   setContainerSize(responseBody_Tag);
+
+  let JQuery_requestElement = document.querySelector('#JQuery_requestElement');
+  let jQuery_requestCode = `$.ajax({
+    url: "https://fakerestapi.in/api/users",
+    type: "POST",
+    data: {
+      "name": "Pankaj Aggarwal",
+      "job": "Team Leader"
+    },
+    success: function(response){
+        console.log(response);
+    }
+  });`;
+  setTextToPreTag(JQuery_requestElement, jQuery_requestCode);
+
+  let JQuery_responseElement = document.querySelector('#JQuery_responseElement');
+  let JQuery_responseCode = `
+  {
+    "name": "Pankaj Aggarwal",
+    "job": "Team Leader",
+    "status": "success",
+    "message": "User created successfully",
+    "createdAt": "Sat, 17 Apr 2021 20:36:23 GMT"
+  }`;
+  setTextToPreTag(JQuery_responseElement, JQuery_responseCode);
+
+  let Javascript_requestElement = document.querySelector('#Javascript_requestElement');
+  let Javascript_requestCode = `
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:1818/api/resources/todos/6", true);
+  xhr.onload = function(){
+      console.log(xhr.response);
+  };
+  xhr.send();`;
+  setTextToPreTag(Javascript_requestElement, Javascript_requestCode);
+
+  let Javascript_responseElement = document.querySelector('#Javascript_responseElement');
+  let Javascript_responseCode = `
+  {
+    "total_todos": 1,
+    "data": [
+      {
+        "id": 6,
+        "userid": "11141XX14",
+        "title": "Recharge dad's mobile",
+        "isCompleted": "true"
+      }
+    ]
+  }`;
+  setTextToPreTag(Javascript_responseElement, Javascript_responseCode);  
 })();
 
 (function(){
@@ -39,7 +89,7 @@ const bodyObject = {};
             </div>
             `;
           });
-          
+
           count += 1;
         }
       }
@@ -164,4 +214,8 @@ function setIdAttribute(__resp){
 
 function checkZero(count){
   if(count>0) return "hide";
+}
+
+function setTextToPreTag(elementId, code){
+  elementId.textContent = code;
 }
