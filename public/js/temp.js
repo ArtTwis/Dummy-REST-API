1f20066387a7cc347bb9ad067585267a6fdaa4d5
Twinkle Goyal <art.twis@gmail.com>
2021-05-07 15:36:30 +0530

    added temp file

---
+++ public/js/temp.js
@@ -0,0 +1,49 @@
+// let JQuery_requestElement = document.querySelector('#JQuery_requestElement');
+// let jQuery_requestCode = `$.ajax({
+//   url: "https://fakerestapi.in/api/users",
+//   type: "POST",
+//   data: {
+//     "name": "Pankaj Aggarwal",
+//     "job": "Team Leader"
+//   },
+//   success: function(response){
+//       console.log(response);
+//   }
+// });`;
+// setTextToPreTag(JQuery_requestElement, jQuery_requestCode);
+
+// let JQuery_responseElement = document.querySelector('#JQuery_responseElement');
+// let JQuery_responseCode = `
+// {
+//   "name": "Pankaj Aggarwal",
+//   "job": "Team Leader",
+//   "status": "success",
+//   "message": "User created successfully",
+//   "createdAt": "Sat, 17 Apr 2021 20:36:23 GMT"
+// }`;
+// setTextToPreTag(JQuery_responseElement, JQuery_responseCode);
+
+// let Javascript_requestElement = document.querySelector('#Javascript_requestElement');
+// let Javascript_requestCode = `
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "http://localhost:1818/api/resources/todos/6", true);
+// xhr.onload = function(){
+//     console.log(xhr.response);
+// };
+// xhr.send();`;
+// setTextToPreTag(Javascript_requestElement, Javascript_requestCode);
+
+// let Javascript_responseElement = document.querySelector('#Javascript_responseElement');
+// let Javascript_responseCode = `
+// {
+//   "total_todos": 1,
+//   "data": [
+//     {
+//       "id": 6,
+//       "userid": "11141XX14",
+//       "title": "Recharge dad's mobile",
+//       "isCompleted": "true"
+//     }
+//   ]
+// }`;
+// setTextToPreTag(Javascript_responseElement, Javascript_responseCode);
